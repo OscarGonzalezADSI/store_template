48b7e4839e3228e6ea864080a75c57af1c0e12e4
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-06-29 12:11:16 -0500

    Add files via upload

--- controller/controller.category.js
+++ controller/controller.category.js
@@ -4,40 +4,77 @@ category.setAttribute("style","font-weight:bold");
 
 function data_category(data)
 {
-	let cont = 0;
-	data.forEach(i => {
+    let count = 0;
+    data.forEach(i => {
+    
+        const divCategory = document.createElement("div");
+        const aCategory = document.createElement("a");
+        const aCategoryText = document.createTextNode(i.category);
+        const iaCategory = document.createElement("i");
+
+		const nodes = {};
 		
-		const divCategory = document.createElement("div");
-		const aCategory = document.createElement("a");
-		const aCategoryText = document.createTextNode(i.category);
-		const iaCategory = document.createElement("i");
+		nodes.category = category;
+		nodes.divCategory = divCategory;
+		nodes.aCategory = aCategory;
+		nodes.aCategoryText = aCategoryText;
+		nodes.iaCategory = iaCategory;
 		
-		divCategory.setAttribute("class", "w3-bar-block w3-hide w3-padding-large w3-medium");
-		divCategory.setAttribute("id", "demoAcc"+cont);
-		aCategory.setAttribute("class","w3-button w3-block w3-white w3-left-align");
-		aCategory.setAttribute("href","javascript:void(0)");
-		aCategory.setAttribute("onclick","myAccFunc('demoAcc"+cont+"')");
-		iaCategory.setAttribute("class", "fa fa-caret-down");
+		data_categoryItems(nodes, i);
 		
-		i.subcategories.forEach(itext => {
-			
-			const a1divCategory = document.createElement("a");
-			const a1divCategoryText = document.createTextNode(itext);
-			const iadivCategory = document.createElement("i");
-			
-			iadivCategory.setAttribute("class", "fa fa-caret-right w3-margin-right");
-			a1divCategory.setAttribute("class", "w3-bar-item w3-button w3-light-grey");
-			a1divCategory.setAttribute("href", "#");
-			
-			a1divCategory.appendChild(iadivCategory);
-			a1divCategory.appendChild(a1divCategoryText);
-			divCategory.appendChild(a1divCategory);			
-		});
+		setAttributeCategory(nodes, count);
+		appendChildCategory(nodes);
 		
-		aCategory.appendChild(aCategoryText);
-		aCategory.appendChild(iaCategory);
-		category.appendChild(aCategory);
-		category.appendChild(divCategory);
-		cont+=1;
+		count+=1;
 	});
 }
+
+function data_categoryItems(nodes, i)
+{
+    i.subcategories.forEach(itext => {
+
+        const a1divCategory = document.createElement("a");
+        const a1divCategoryText = document.createTextNode(itext.subcategory);
+        const iadivCategory = document.createElement("i");
+        
+        nodes.iadivCategory = iadivCategory;
+        nodes.a1divCategory = a1divCategory;
+        nodes.a1divCategoryText = a1divCategoryText;
+        
+        setAttributeItems(nodes, itext.linkend);
+        appendChildItems(nodes);
+    });
+}
+
+function setAttributeCategory(nodes, count)
+{
+    nodes.divCategory.setAttribute("id", "demoAcc"+count);
+    nodes.divCategory.setAttribute("class", "w3-bar-block w3-hide w3-padding-large w3-medium");
+    nodes.aCategory.setAttribute("class","w3-button w3-block w3-white w3-left-align");
+    nodes.aCategory.setAttribute("href","javascript:void(0)");
+    nodes.aCategory.setAttribute("onclick","myAccFunc('demoAcc"+count+"')");
+    nodes.iaCategory.setAttribute("class", "fa fa-caret-down");
+}
+
+function setAttributeItems(nodes, linkend)
+{   
+    nodes.iadivCategory.setAttribute("class", "fa fa-caret-right w3-margin-right");
+    nodes.a1divCategory.setAttribute("class", "w3-bar-item w3-button w3-light-grey");
+    nodes.a1divCategory.setAttribute("href", linkend);
+}
+
+function appendChildCategory(nodes)
+{
+    nodes.aCategory.appendChild(nodes.aCategoryText);
+    nodes.aCategory.appendChild(nodes.iaCategory);
+    nodes.category.appendChild(nodes.aCategory);
+    nodes.category.appendChild(nodes.divCategory);
+}
+
+function appendChildItems(nodes)
+{
+    nodes.a1divCategory.appendChild(nodes.iadivCategory);
+    nodes.a1divCategory.appendChild(nodes.a1divCategoryText);
+    nodes.divCategory.appendChild(nodes.a1divCategory);
+}
+
--- data/data.category.js
+++ data/data.category.js
@@ -1,10 +1,28 @@
 const data = [
 	{
 		"category" : "blusa",
-		"subcategories" : ["blusa tiras ", "blusa cerrada "]
+		"subcategories" : [
+			{
+				"subcategory" : "blusa tiras ",
+				"linkend" : "http://www.google.com"
+			},
+			{
+				"subcategory" : "blusa cerrada ",
+				"linkend" : "http://www.facebook.com"
+			}
+		]
 	},
 	{
 		"category" : "Jeans",
-		"subcategories" : ["Skinny", "Relaxed"]
+		"subcategories" : [
+			{
+				"subcategory" : "blusa tiras ",
+				"linkend" : "http://www.google.com"
+			},
+			{
+				"subcategory" : "blusa cerrada ",
+				"linkend" : "http://www.facebook.com"
+			}
+		]
 	}
 ];
