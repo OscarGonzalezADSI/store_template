80d4d071f162f63518d9d73ec9d8d2da231bd573
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-06-30 16:29:58 -0500

    Add files via upload

--- js/items.js
+++ js/items.js
@@ -23,3 +23,15 @@ function itemsAppendChild(nodes)
     nodes.a1divCategory.appendChild(nodes.a1divCategoryText);
     nodes.divCategory.appendChild(nodes.a1divCategory);
 }
+
+function countItems(category, subcategory)
+{
+	const divCount = document.getElementById("countItems");
+	const pdivCount = document.createElement("p");
+	const pdivCountText = document.createTextNode(service_countItems(category, subcategory)+" items");
+	
+	divCount.setAttribute("class", "w3-container w3-text-grey");
+	divCount.innerHTML = "";
+	pdivCount.appendChild(pdivCountText);
+	divCount.appendChild(pdivCount);	
+}
